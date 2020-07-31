899c8d437b594ff6c1ac2cd576fe24f7c23e7a92
Jack Murray <jackmurray445@gmail.com>
2020-07-30 23:48:44 -0700

    Rewrote following portion of user model

--- app/User.php
+++ app/User.php
@@ -70,12 +70,12 @@ class User extends Authenticatable implements JWTSubject
 
     public function followers()
     {
-        return $this->hasMany('App\Follow', 'followed_user_id');
+        return $this->belongsToMany('App\User', 'follows', 'followed_user_id', 'following_user_id');
     }
     
     public function following()
     {
-        return $this->hasMany('App\Follow', 'following_user_id');
+        return $this->belongsToMany('App\User', 'follows', 'following_user_id', 'followed_user_id');
     }
 
     public function getJWTIdentifier()
